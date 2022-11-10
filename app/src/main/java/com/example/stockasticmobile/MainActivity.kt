bbe82e8eb34fd705b7018413841f92d2279aac7d
draganoid16 <draganoid5@live.com>
2022-11-10 21:43:46 +0000

    fixed layout, added partial basic login screen

--- app/src/main/java/com/example/stockasticmobile/MainActivity.kt
+++ app/src/main/java/com/example/stockasticmobile/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.stockasticmobile
 
+import android.app.Activity
 import android.graphics.drawable.AnimationDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //sleeps for 2s and activates splash screen from google api
-        Thread.sleep(2000)
+
+
+        //sleeps for 1s and activates splash screen from google api
+        Thread.sleep(1000)
         val splashScreen = installSplashScreen()
         setContentView(R.layout.activity_main)
         //animation
@@ -18,11 +24,15 @@ class MainActivity : AppCompatActivity() {
 
         val animationDrawable: AnimationDrawable = constraintLayout.background as AnimationDrawable
 
-        animationDrawable.setEnterFadeDuration(2500)
+        animationDrawable.setEnterFadeDuration(1500)
 
-        animationDrawable.setExitFadeDuration(5000)
+        animationDrawable.setExitFadeDuration(3000)
 
         animationDrawable.start()
+
+        //hide action bar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide()
     }
 
 
